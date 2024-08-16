e6595a1b5b52ad773156b00acda4c3164b62c50f
Omkar-Primocys <om.primocys@gmail.com>
2024-08-16 19:05:43 +0530

    User List Design is done. Reported User List Design Changes is Pending with data tables

--- src/components/User/UserDetails.tsx
+++ src/components/User/UserDetails.tsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Swal from 'sweetalert2';
 import useApiPost from "../../hooks/PostData";
 import { Link } from 'react-router-dom';
-import { DataTable, DataTableSortStatus } from 'mantine-datatable';
-import { profile } from 'console';
+import { DataTable } from 'mantine-datatable';
 
 interface User {
     user_id: number;
@@ -12,10 +11,10 @@ interface User {
     first_name: string;
     last_name: string;
     dob: string;
-    Country_flag:string;
-    combined_num:string;
-    country_code : string;
-    mobile_num : number;
+    Country_flag: string;
+    combined_num: string;
+    country_code: string;
+    mobile_num: number;
     email_id: string;
     password: string;
     login_type: string;
@@ -26,8 +25,13 @@ interface User {
     updatedAt: string;
 }
 
+interface Pagenation {
+    total: number;
+
+}
 const UserList: React.FC = () => {
     const [users, setUsers] = useState<User[]>([]);
+    const [pagenations, setPegenations] = useState<Pagenation[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
     const [currentPage, setCurrentPage] = useState<number>(1);
@@ -41,6 +45,7 @@ const UserList: React.FC = () => {
             const response = await postData('getAllUser', { page: currentPage, limit });
             if (response && response.allUsers) {
                 setUsers(response.allUsers);
+                setPegenations(response.pagination)
                 setTotalPages(response.pagination.pages);
             }
         } catch (err: any) {
@@ -68,7 +73,7 @@ const UserList: React.FC = () => {
             cancelButtonText: 'No, keep it',
             customClass: {
                 confirmButton: 'bg-yellow-500 text-white',
-                cancelButton: 'bg-gray-300 text-black'
+                cancelButtonText: 'bg-gray-300 text-black',
             },
             padding: '2em',
         });
@@ -86,7 +91,9 @@ const UserList: React.FC = () => {
             }
         }
     };
-
+    const createTitle = (title: string) => (
+        <div className="font-bold p-2">{title}</div> // Tailwind CSS classes applied
+    );
     const handleDelete = async (user: User) => {
         const result = await Swal.fire({
             icon: 'question',
@@ -97,7 +104,7 @@ const UserList: React.FC = () => {
             cancelButtonText: 'No, keep it',
             customClass: {
                 confirmButton: 'bg-red-500 text-white',
-                cancelButton: 'bg-gray-300 text-black'
+                cancelButtonText: 'bg-gray-300 text-black',
             },
             padding: '2em',
         });
@@ -117,13 +124,10 @@ const UserList: React.FC = () => {
         }
     };
 
-    if (loading) return (
-        <div className="min-h-[325px] grid place-content-center bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08]">
-            <span className="animate-spin border-2 border-black dark:border-white !border-l-transparent rounded-full w-5 h-5 inline-flex"></span>
-        </div>
-    );
-    if (error) return <p>{error}</p>;
-
+    if (loading) return <span className="animate-spin border-[3px] border-transparent border-l-primary rounded-full w-6 h-6 inline-block align-middle m-auto mb-10"></span>
+        ;
+    if (error) return <div>Error occurred!</div>;
+    let a = 12
     return (
         <div className="mt-6">
             <ul className="flex space-x-2 rtl:space-x-reverse text-sm text-gray-700">
@@ -139,11 +143,11 @@ const UserList: React.FC = () => {
             <div className='panel mt-6'>
                 <div className="flex justify-between items-center mb-5">
                     <h5 className="font-semibold text-lg dark:text-white-light">User List</h5>
-                    <div className="flex items-center space-x-2">
+                    {/* <div className="flex items-center space-x-2">
                         <label htmlFor="limit" className="font-semibold dark:text-white-light">
                             Limit:
                         </label>
-                        <select
+                        <select 
                             id="limit"
                             value={limit}
                             onChange={(e) => setLimit(Number(e.target.value))}
@@ -153,111 +157,159 @@ const UserList: React.FC = () => {
                             <option value={20}>20</option>
                             <option value={50}>50</option>
                         </select>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="panel mt-6">
-                <div className="datatables">
-                    <DataTable
-                        noRecordsText="No results match your search query"
-                        highlightOnHover
-                        className="whitespace-nowrap table-hover !overflow-auto"
-                        records={users}
-                        columns={[
-                            { accessor: 'id', title: 'ID', 
-                                sortable: true, 
-                                render: ({ user_id }) => (
-                                    <strong className="text-info">#{user_id}</strong>
-                                ),
-                            },
-                            {
-                                accessor: 'firstName',
-                                title: 'User',
-                                sortable: true,
-                                render: ({ first_name, last_name ,profile_pic }) => (
-                                    <div className="flex items-center gap-2">
-                                        <img src={profile_pic} className="w-9 h-9 rounded-full max-w-none" alt="user-profile" />
-                                        <div className="font-semibold">{first_name + ' ' + last_name}</div>
-                                    </div>
-                                ),
-                            },
-                            {
-                                accessor: 'country',
-                                title: 'Country',
-                                render: ({Country_flag}) => (
-                                    <div className="flex items-center gap-2">
-                                        <img width="24" src={`/assets/images/flags/${Country_flag.substring(0, 2).toUpperCase()}.svg`} className="max-w-none" alt="flag" />
-                                        <div>{Country_flag.substring(0, 2).toUpperCase()}</div>
-                                    </div>
-                                ),
-                            },
-                            {
-                                accessor: 'email',
-                                title: 'Email',
-                                sortable: true,
-                                render: ({ email_id }) => (
-                                    <a href={`mailto:${email_id}`} className="text-primary hover:underline">
-                                        {email_id}
-                                    </a>
-                                ),
-                            },
-                            {
-                                accessor: 'phone',
-                                title: 'Phone',
-                                render: ({country_code,mobile_num}) => (
+                    <div className="datatables">
+
+                        <DataTable
+                            highlightOnHover
+                            className="whitespace-nowrap table-hover !overflow-auto "
+                            records={users}
+                            columns={[
+                                {
+                                    accessor: 'user_id',
+                                    title: createTitle('ID'),
+                                    // sortable: true,
+                                    render: ({ user_id }) => (
+                                        <strong className="text-info">#{user_id}</strong>
+                                    ),
+                                },
+                                {
+                                    accessor: 'firstName',
+                                    title: createTitle('Full Name'),
+                                    // sortable: true,
+                                    render: ({ first_name, last_name, profile_pic }) => (
+                                        <div className="flex items-center gap-2">
+                                            <img src={profile_pic} className="w-9 h-9 rounded-full max-w-none" alt="user-profile" />
+                                            <div className="font-semibold">{first_name + ' ' + last_name}</div>
+                                        </div>
+                                    ),
+                                },
+                                {
+                                    accessor: 'user_name',
+                                    title: createTitle('User Name'),
+                                    render: ({ user_name }) => (
                                         <div className='flex gap-2'>
+                                            <div>{user_name}</div>
+                                        </div>
+                                    ),
+                                },
+                                {
+                                    accessor: 'country',
+                                    title: createTitle('Country'),
+                                    render: ({ Country_flag }) => (
+                                        <div className="flex items-center gap-2">
+                                            <img width="24" src={`/assets/images/flags/${Country_flag.substring(0, 2).toUpperCase()}.svg`} className="max-w-none" alt="flag" />
+                                            <div>{Country_flag.substring(0, 2).toUpperCase()}</div>
+                                        </div>
+                                    ),
+                                },
+                                {
+                                    accessor: 'email',
+                                    title: createTitle('Email'),
+                                    // sortable: true,
+                                    render: ({ email_id }) => (
+                                        <a href={`mailto:${email_id}`} className="text-primary hover:underline">
+                                            {email_id}
+                                        </a>
+                                    ),
+                                },
+                                {
+                                    accessor: 'phone',
+                                    title: createTitle('Phone No.'),
+                                    render: ({ country_code, mobile_num }) => (
+                                        <div className='flex gap-2 '>
                                             <div>{country_code}</div>
                                             <div>{mobile_num}</div>
-
                                         </div>
-                                ),
-                            },
-                            // { accessor: 'phone', title: 'Phone', sortable: true },
-                            // {
-                            //     accessor: 'rating',
-                            //     title: 'Rate',
-                            //     titleClassName: '!text-center',
-                            //     render: ({ id }) => (
-                            //         <div className="flex items-center justify-center text-warning">
-                            //             {Array.from(Array(getRandomNumber(1, 5)).keys()).map((i) => {
-                            //                 return <IconStar key={i + id} className=" fill-warning" />;
-                            //             })}
-                            //         </div>
-                            //     ),
-                            // },
-                            // {
-                            //     accessor: 'series',
-                            //     title: 'Progress',
-                            //     render: ({ id }) => (
-                            //         <ReactApexChart
-                            //             key={id}
-                            //             type="line"
-                            //             series={[{ data: [21, 9, 36, 12, 44, 25, 59] }]}
-                            //             // @ts-ignore
-                            //             options={chart_options()}
-                            //             height={30}
-                            //             width={150}
-                            //         />
-                            //     ),
-                            // },
-                            // {
-                            //     accessor: 'status',
-                            //     title: 'Status',
-                            //     render: () => <span className={`badge badge-outline-${randomStatusColor()} `}>{randomStatus()}</span>,
-                            // },
-                        ]}
-                        // totalRecords={initialRecords.length}
-                        // recordsPerPage={pageSize}
-                        // page={page}
-                        // onPageChange={(p) => setPage(p)}
-                        // recordsPerPageOptions={PAGE_SIZES}
-                        // onRecordsPerPageChange={setPageSize}
-                        // sortStatus={sortStatus}
-                        // onSortStatusChange={setSortStatus}
-                        // minHeight={200}
-                        // paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
-                    />
+                                    ),
+                                },
+                                {
+                                    accessor: 'verified',
+                                    title: createTitle('Verification Status'),
+                                    render: ({ verified }) => (
+                                        <div className={`flex justify-center text-[12px] ${verified ? 'text-success p-0.5 w-20 hover:bg-green-100  border-success border-[1px] rounded-lg ml-5' : 'text-danger p-0.5 w-28 ml-2 border-danger hover:bg-red-100 border-[1px] rounded-lg'}`}>
+                                            <div>{verified ? 'Approved' : 'Not Verified'}</div>
+                                        </div>
+                                    ),
+                                },
+                            ]}
+                            minHeight={200}
+
+                        />
+
+                        <div className="flex mt-4 justify-between items-center">
+
+
+                            {/* Pagination Controls */}
+                            <div className="flex items-center text-sm text-dark dark:text-white-light">
+                                <span className='font-semibold' >
+                                    Showing {((currentPage - 1) * limit) + 1} to {Math.min(currentPage * limit, pagenations.total)} of {pagenations.total} entries
+                                </span>
+                                <select
+                                    value={limit}
+                                    onChange={(e) => setLimit(Number(e.target.value))}
+                                    className="appearance-none text-dark border-gray-300 border-[1px] rounded-md dark:bg-[#191e3a] dark:text-white-light ml-2 px-4 py-0.5 focus:outline-none  "
+                                >
+                                    {[10, 20, 50].map(option => (
+                                        <option
+                                            className="bg-white dark:bg-[#191e3a] dark:text-white-light text-dark p-2 hover:bg-gray-200 dark:hover:bg-[#30375e]  dark:focus:bg-[#30375e] rounded-lg"
+                                            key={option}
+                                            value={option}
+                                        >
+                                            {option}
+                                        </option>
+                                    ))}
+                                </select>
+
+                            </div>
+
+
+                            {/* Pagination Buttons */}
+                            <ul className="inline-flex items-center space-x-1 rtl:space-x-reverse">
+                                {/* Previous Button */}
+                                <li>
+                                    <button
+                                        type="button"
+                                        onClick={() => setCurrentPage(prevPage => Math.max(prevPage - 1, 1))}
+                                        disabled={currentPage === 1}
+                                        className="flex justify-center font-semibold p-2 rounded-full transition bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+                                    >
+                                        <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                                        </svg>
+                                    </button>
+                                </li>
+                                {/* Page Number Buttons */}
+                                {[...Array(totalPages)].map((_, i) => (
+                                    <li key={i}>
+                                        <button
+                                            type="button"
+                                            onClick={() => setCurrentPage(i + 1)}
+                                            className={`flex justify-center font-semibold px-3.5 py-2 rounded-full transition ${currentPage === i + 1 ? 'bg-primary text-white' : 'bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary'}`}
+                                        >
+                                            {i + 1}
+                                        </button>
+                                    </li>
+                                ))}
+                                {/* Next Button */}
+                                <li>
+                                    <button
+                                        type="button"
+                                        onClick={() => setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages))}
+                                        disabled={currentPage === totalPages}
+                                        className="flex justify-center font-semibold p-2 rounded-full transition bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+                                    >
+                                        <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                                        </svg>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
